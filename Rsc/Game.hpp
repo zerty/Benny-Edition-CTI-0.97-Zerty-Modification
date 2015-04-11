893aafb17803668146e9ee0389fedc2aa680541c
zerty <zerty@crans.org>
2015-04-11 10:52:53 +0200

    Changing prerelease number

--- Rsc/Game.hpp
+++ Rsc/Game.hpp
@@ -4,11 +4,11 @@ respawnDelay = 10;
 respawnDialog = false;
 
 //--- Require briefing.html to show up.
-onLoadMission = "BECTI 0.97 - Zerty 1.3.1.0 [PreRelease2]";
+onLoadMission = "BECTI 0.97 - Zerty 1.3.1.0 [PreRelease3]";
 onLoadMissionTime = false;
-onLoadName = "BECTI 0.97 - Zerty 1.3.1.0 [PreRelease2]";
+onLoadName = "BECTI 0.97 - Zerty 1.3.1.0 [PreRelease3]";
 
-briefingName = "BECTI 0.97 - Zerty 1.3.1.0 [PreRelease2]";
+briefingName = "BECTI 0.97 - Zerty 1.3.1.0 [PreRelease3]";
 
 //--- ArmA 3 Specifics
 dev = "Benny";
