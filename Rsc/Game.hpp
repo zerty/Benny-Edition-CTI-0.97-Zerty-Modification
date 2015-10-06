a43a3580f5c76b7b78db1d8e0747b177ffd9d8e0
zerty <zerty@crans.org>
2015-10-06 12:18:13 +0200

    Version Number #80

--- Rsc/Game.hpp
+++ Rsc/Game.hpp
@@ -4,11 +4,11 @@ respawnDelay = 10;
 respawnDialog = false;
 
 //--- Require briefing.html to show up.
-onLoadMission = "BECTI 0.97 - Zerty 1.3.1.1 [preRelease]";
+onLoadMission = "BECTI 0.97 - Zerty 1.3.2.0";
 onLoadMissionTime = false;
-onLoadName = "BECTI 0.97 - Zerty 1.3.1.1 [preRelease]";
+onLoadName = "BECTI 0.97 - Zerty 1.3.2.0";
 
-briefingName = "BECTI 0.97 - Zerty 1.3.1.1 [preRelease]";
+briefingName = "BECTI 0.97 - Zerty 1.3.2.0";
 
 //--- ArmA 3 Specifics
 dev = "Benny";
