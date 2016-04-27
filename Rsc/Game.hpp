d1073a428ba7e4c218bc48c3fb41c8961ae00e60
zerty <zerty@crans.org>
2016-04-27 20:53:24 +0200

    Changing mission version
    just because I  can

--- Rsc/Game.hpp
+++ Rsc/Game.hpp
@@ -4,11 +4,11 @@ respawnDelay = 10;
 respawnDialog = false;
 
 //--- Require briefing.html to show up.
-onLoadMission = "BECTI 0.97 - Zerty 1.3.2.0";
+onLoadMission = "BECTI 0.97 - Zerty 1.3.2.1";
 onLoadMissionTime = false;
-onLoadName = "BECTI 0.97 - Zerty 1.3.2.0";
+onLoadName = "BECTI 0.97 - Zerty 1.3.2.1";
 
-briefingName = "BECTI 0.97 - Zerty 1.3.2.0";
+briefingName = "BECTI 0.97 - Zerty 1.3.2.1";
 
 //--- ArmA 3 Specifics
 dev = "Benny";
