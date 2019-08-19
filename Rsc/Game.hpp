1ec631620102991f8c091ed5d4094088f8b6f6a0
Yoshi-E <server19960714@yahoo.de>
2019-08-19 12:42:20 +0200

    Updated version number

--- Rsc/Game.hpp
+++ Rsc/Game.hpp
@@ -4,11 +4,11 @@ respawnDelay = 10;
 respawnDialog = false;
 
 //--- Require briefing.html to show up.
-onLoadMission = "BECTI - Zerty/HTom/Yoshi_E 1.3.5.0";
+onLoadMission = "BECTI - Zerty/HTom/Yoshi_E 1.3.5.1";
 onLoadMissionTime = false;
 onLoadName = "BECTI - Zerty/HTom/Yoshi_E";
 
-briefingName = "BECTI - Zerty/HTom/Yoshi_E 1.3.5.0";
+briefingName = "BECTI - Zerty/HTom/Yoshi_E 1.3.5.1";
 
 //--- ArmA 3 Specifics
 dev = "Benny";
