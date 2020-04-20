0c06b4f24d6f6b5d41976e8ac3e7f19ec242c605
Zerty <4232921+zerty@users.noreply.github.com>
2020-04-20 16:14:15 +0200

    Version Up

--- Rsc/Game.hpp
+++ Rsc/Game.hpp
@@ -4,11 +4,11 @@ respawnDelay = 10;
 respawnDialog = false;
 
 //--- Require briefing.html to show up.
-onLoadMission = "BECTI - Zerty/HTom/Yoshi_E 1.3.5.1";
+onLoadMission = "BECTI - Zerty/HTom/Yoshi_E 1.3.5.2";
 onLoadMissionTime = false;
 onLoadName = "BECTI - Zerty/HTom/Yoshi_E";
 
-briefingName = "BECTI - Zerty/HTom/Yoshi_E 1.3.5.1";
+briefingName = "BECTI - Zerty/HTom/Yoshi_E 1.3.5.2";
 
 //--- ArmA 3 Specifics
 dev = "Benny";
